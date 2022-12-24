f2da27a16511fc4405938aebf673dbf5e1e70a8f
iLOSTinYOU <87181511+orekixadi@users.noreply.github.com>
2022-12-24 12:04:11 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -94,7 +94,7 @@ async def gen_thumb(videoid):
 
         crop_img = Image.open(f"cache/cropped{videoid}.png")
         logo = crop_img.convert("RGBA")
-        logo.thumbnail((190, 190), Image.ANTIALIAS)
+        logo.thumbnail((190, 180), Image.ANTIALIAS)
         width = int((1280 - 180)/ 1)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 2, 134), mask=logo)
